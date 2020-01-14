0424f1dd8ca075dac5be3b725e85b812f07cd416
Kritik Jain <43483330+kjain0073@users.noreply.github.com>
2020-01-15 03:11:10 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+ export const robots = [
+{
+	id:1,
+	name:'Ron Van Dyre',
+	username: 'rondye',
+	email: 'rondye@gmail.com'
+},
+{
+	id:2,
+	name:'William Daniels',
+	username: 'danwilliam',
+	email: 'danwillam@gmail.com'
+},
+{
+	id:3,
+	name:'Robert Mckay',
+	username: 'robbiemac',
+	email: 'robbiemac@gmail.com'
+},
+{
+	id:4,
+	name:'Quinton Mckoy',
+	username: 'macquintie',
+	email: 'macquintie@gmail.com'
+},
+{
+	id:5,
+	name:'Nicole Dugman',
+	username: 'dugnicole',
+	email: 'dugnicole@gmail.com'
+},
+{
+	id:6,
+	name:'Steve Patrick',
+	username: 'petsteve',
+	email: 'petsteve@gmail.com'
+},
+{
+	id:7,
+	name:'Michael Stark',
+	username: 'mrstark',
+	email: 'mrstark@gmail.com'
+},
+
+{
+	id:8,
+	name:'Tony Rodriguez',
+	username: 'notmrstark',
+	email: 'notmrstark@gmail.com'
+},
+{
+	id:9,
+	name:'Richard Mcmillan',
+	username: 'richmac',
+	email: 'richmac@gmail.com'
+},
+{
+	id:10,
+	name:'Dan Rich',
+	username: 'richiedan',
+	email: 'richiedan@gmail.com'
+}
+ ]
